197f1c9a5d2dd44612e99fa9456b0a3e171abaa4
Melody Lynn <mysticcreeper99@gmail.com>
2021-06-20 17:05:49 +1000

    Update 3
    Added a response system

--- Assets/Scripts/DialogueUI.cs
+++ Assets/Scripts/DialogueUI.cs
@@ -9,11 +9,14 @@ public class DialogueUI : MonoBehaviour
     [SerializeField] private DialogueObject testDialogue;
     [SerializeField] private GameObject dialogueBox;
 
+    private ResponseHandler responseHandler;
     private TypewriterEffect typewriterEffect;
     
     private void Start()
     {
         typewriterEffect = GetComponent<TypewriterEffect>();
+        responseHandler = GetComponent<ResponseHandler>();
+
         CloseDialogueBox();
         ShowDialogue(testDialogue);
     }
@@ -24,15 +27,27 @@ public class DialogueUI : MonoBehaviour
         StartCoroutine(StepThroughDialogue(dialogueobject));
     }
 
-    private IEnumerator StepThroughDialogue(DialogueObject dialogueobject)
+    private IEnumerator StepThroughDialogue(DialogueObject dialogueObject)
     {
-        foreach(string dialogue in dialogueobject.Dialogue)
+        for (int i = 0; i < dialogueObject.Dialogue.Length; i++)
         {
+            string dialogue = dialogueObject.Dialogue[i];
             yield return typewriterEffect.Run(dialogue, textLabel);
+
+            if (i == dialogueObject.Dialogue.Length - 1 && dialogueObject.HasResponses) break;
+
             yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
         }
 
-        CloseDialogueBox();
+        if(dialogueObject.HasResponses)
+        {
+            responseHandler.ShowResponses(dialogueObject.Responses);
+        }
+        else
+        {
+            CloseDialogueBox();
+        }
+
     }
 
     private void CloseDialogueBox()
